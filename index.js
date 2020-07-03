4bb2eca3ac6d0f51e6bed51db2b5fb9bad3b6185
Frederic Mercille <fmercille@gmail.com>
2020-07-02 23:27:40 -0400

    Exercises 3.5 and 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let entries = [
   {
     "name": "Arto Hellas",
@@ -36,6 +38,32 @@ app.get('/api/persons/:id', (req, res) => {
     res.json(entry)
   } else {
     res.status(404).end()
+  }
+})
+
+app.post('/api/persons', (req, res) => {
+  const name = req.body.name
+  const number = req.body.number
+  const id = Math.ceil(Math.random() * 1e9)
+
+  if (!name) {
+    res.status(400).json({ 'error': 'name missing' }).end()
+  } else if (!number) {
+    res.status(400).json({ 'error': 'number missing' }).end()
+  }
+  else if (entries.find(entry => entry.name.toLowerCase() === name.toLowerCase())) {
+    res.status(409).json({ 'error': 'name must be unique' }).end()
+  } else {
+    if (name && number) {
+      const newEntry = {
+        "name": name,
+        "number": number,
+        "id": id
+      }
+      entries.push(newEntry)
+
+      res.status(204).end()
+    }
   }
 })
 
