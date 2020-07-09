0d9b2fcfebf13afaade1c64f9a3809a68f6ba835
Frederic Mercille <fmercille@gmail.com>
2020-07-08 22:30:20 -0400

    Use PORT environment variable for the server

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.get('/info', (req, res) => {
   res.send(`<p>${infoline}</p><p>${new Date().toString()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
