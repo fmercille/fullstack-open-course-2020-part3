e9f67cb641638e145fbd85f4fb2acaf25a3efef1
Frederic Mercille <fmercille@gmail.com>
2020-07-08 22:59:08 -0400

    uibuild

---
+++ build/precache-manifest.4a60d9ee2efeb8ec2501aa8cc9559b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0755f7d282fe30010a29882744d247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38d78396c7d81b127c1c",
+    "url": "/static/css/main.71823fdb.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d78396c7d81b127c1c",
+    "url": "/static/js/main.60cdd891.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
